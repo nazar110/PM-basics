6f91f72c5f2dd6267590e7656dd1778f08ff72ec
nazar110 <nazar1124@gmail.com>
2021-08-27 02:12:32 +0300

    Add some functionality

--- site.js
+++ site.js
@@ -1,3 +1,13 @@
-function scrollTo(hash) {
-    location.hash = "#" + hash;
+window.onscroll = function() { stick() };
+function stick()
+{
+    var header = document.getElementById("myheader");
+    var newPosition = header.offsetTop;
+
+    if(window.pageYOffset > newPosition) {
+        header.classList.add("sticky");
+    }
+    else {
+        header.classList.remove("sticky");
+    }
 }
