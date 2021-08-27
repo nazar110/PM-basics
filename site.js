ebb65792350e490415774c324aab069df5a1055e
nazar110 <nazar1124@gmail.com>
2021-08-27 03:20:31 +0300

    Create a smooth scrolling effect

--- site.js
+++ site.js
@@ -11,3 +11,24 @@ function stick()
         header.classList.remove("sticky");
     }
 }
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if(this.hash !== "") {
+            //event.preventDefault();
+            
+            var hash = this.hash;
+
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset.top()
+                },
+                800,
+                function() {
+                    window.location.hash = hash;
+                }
+            );
+        }
+
+    });
+});
